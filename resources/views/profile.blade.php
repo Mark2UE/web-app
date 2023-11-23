a1065035e0af58fc5a973abefe4766129976f727
Mark2UE <manliclic.markbernard@ue.edu.ph>
2023-11-23 11:00:09 +0800

    user profile option form (initial)

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,34 @@
+@include('partials.header')
+<title>User Page</title>
+<div class="hero-new__background-image-user d-flex align-items-center">
+
+    <div class="container-xxl">
+        <div class="row m-5">
+            <div class="col-lg-12 text-center p-5">
+
+                <h1 class="fs-1">
+                    <br>
+                    <b>User Profile</b>
+                </h1>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<section class="custom-margin-10">
+    <div class="row">
+        <div class="col">
+            <h1>Profile</h1>
+            <hr>
+        </div>
+    </div>
+
+
+
+</section>
+
+
+
+@include('partials.footer')
