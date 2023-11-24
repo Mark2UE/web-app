4acfa9def188d63bce2ffd8576bf0fbbd91046ec
Mark2UE <manliclic.markbernard@ue.edu.ph>
2023-11-25 06:22:13 +0800

    Migration Commnt

---
+++ database/migrations/2023_11_25_014815_comment.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('connected_to'); //CONNNECTS TO POST ID
+            $table->string('commented_to'); //CONNECTS TO EMAIL or USER
+            $table->text('comment');
+            $table->string('author');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
