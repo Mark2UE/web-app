6d58dd6dc701d8e88f2026ce616412c37c046d7c
Mark2UE <manliclic.markbernard@ue.edu.ph>
2023-11-27 10:02:36 +0800

    Users can comment on posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Post as PostModel;
 use Illuminate\Http\Request;
 use Post;
 use Illuminate\Support\Facades\DB;
+use App\Models\Comment as Commenttopost;
+use Comment;
 
 class PostController extends Controller
 {
@@ -44,11 +46,26 @@ class PostController extends Controller
             ->join('users', 'posts.author', '=', 'users.email')
             ->where('posts.status', '=', 'Public')
             ->where('posts.id', '=', $id)
-            ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture')
+            ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture', 'users.email')
             ->orderBy('posts.created_at', 'desc')
             ->first(); // Use first() instead of get() to retrieve a single record
 
-        return view('zencom.post', ['post' => $post]);
+        $comment = DB::table('comments')
+            ->join('users', 'users.email', '=', 'author')
+
+            ->where('comments.connected_to', '=', $id)
+            ->select('comments.commented_to', 'comments.id', 'comments.connected_to', 'comments.comment', 'comments.author', 'comments.created_at', 'users.name', 'users.picture', 'users.email')
+            ->orderBy('comments.created_at', 'desc')
+            ->get();
+
+
+
+
+        return view('zencom.post', [
+            'post' => $post,
+            'comments' => $comment,
+
+        ]);
     }
 
 
@@ -58,9 +75,122 @@ class PostController extends Controller
             ->join('users', 'posts.author', '=', 'users.email')
             ->where('posts.status', '=', 'Public')
             ->where('posts.author', '=', auth()->user()->email)
-            ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture')
+            ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture', 'users.email')
             ->orderBy('posts.created_at', 'desc')
             ->get(); // Use first() instead of get() to retrieve a single record
-        return view('zencom.profile', ['posts' => $post]);
+
+        $interaction = DB::table('comments')
+            ->select('comment')
+            ->where('comments.author', '=', auth()->user()->email)
+            ->count();
+
+        $contribute = DB::table('posts')
+            ->select('body')
+            ->where('author', '=', auth()->user()->email)
+            ->count();
+
+
+
+        return view(
+            'zencom.profile',
+            [
+                'posts' => $post,
+                'interactions' => $interaction,
+                'contribute' => $contribute
+
+            ]
+
+
+        );
+    }
+
+
+    public function comment(Request $request, $id)
+    {
+
+        if (auth()->user()->id == $id) {
+
+            $validatedData = $request->validate([
+
+                'comment' => 'required',
+                'connected_to' => 'required',
+                'commented_to' => 'required',
+            ]);
+
+
+            $Comment = Commenttopost::create([
+                'comment' => $validatedData['comment'],
+                'connected_to' => $validatedData['connected_to'],
+                'commented_to' => $validatedData['commented_to'],
+                'author' => auth()->user()->email,
+                'status' => "PUBLIC",
+
+            ]);
+            $id = $validatedData['connected_to'];
+            if ($Comment) {
+                return redirect('/community/view/' . $id . '')->with('messagegreen', 'Your Comment has been posted.');
+            } else {
+                return back()->with('message', 'Error post.');
+            }
+        }
+    }
+
+
+    public function delete_post($id)
+    {
+        $matchingRowsTable1 = PostModel::where('id', $id)->get();
+        foreach ($matchingRowsTable1 as $row) {
+            $row->delete();
+        }
+        $matchingRowsTable2 = Commenttopost::where('connected_to', $id)->get();
+        foreach ($matchingRowsTable2 as $row) {
+            $row->delete();
+        }
+        return back()->with('messagegreen', 'Posts and Comments has been Deleted.');
+    }
+
+    public function view_profile($id)
+    {
+
+        if ($id == auth()->user()->email) {
+            return redirect('/community/profile');
+        } else {
+
+            $user = DB::table('users')
+                ->select('*')
+                ->where('email', '=', $id)
+                ->first();
+
+            $post = DB::table('posts')
+                ->join('users', 'posts.author', '=', 'users.email')
+                ->where('posts.status', '=', 'Public')
+                ->where('posts.author', '=', $id)
+                ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture', 'users.email')
+                ->orderBy('posts.created_at', 'desc')
+                ->get(); // Use first() instead of get() to retrieve a single record
+
+            $interaction = DB::table('comments')
+                ->select('comment')
+                ->where('comments.author', '=', $id)
+                ->count();
+
+            $contribute = DB::table('posts')
+                ->select('body')
+                ->where('author', '=', $id)
+                ->count();
+
+
+
+            return view(
+                'zencom.users',
+                [
+                    'posts' => $post,
+                    'interactions' => $interaction,
+                    'contribute' => $contribute,
+                    'user' => $user,
+
+                ]
+            );
+        }
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,44 @@ use Illuminate\Hashing;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\View;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Contracts\Mail\Mailer;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use PhpParser\Lexer\TokenEmulator\TokenEmulator;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
+
+    public function index()
+    {
+        $posts = DB::table('users')
+            ->select('users.email')
+            ->get();
+        $users = DB::table('users')
+            ->select('users.name')
+            ->get();
+        $takenusers = $users->pluck('name')->toArray();
+        $takenEmails = $posts->pluck('email')->toArray();
+        $user_lower = array_map('strtolower', $takenusers);
+        $email_lower = array_map('strtolower', $takenEmails);
+        return view(
+            'welcome',
+            [
+                'takenEmails' => json_encode($email_lower),
+                'takenUsers' => json_encode($user_lower)
+            ]
+
+        );
+    }
+
     public function register(Request $request)
     {
+
 
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
@@ -25,12 +54,20 @@ class UserController extends Controller
             'password' => 'required|string|min:6|confirmed',
         ]);
 
+        // Convert 'name' and 'email' to lowercase
+        $user_lower = strtolower($validatedData['name']);
+        $email_lower = strtolower($validatedData['email']);
+
+        // Concatenate '@' with lowercase 'name'
+
+
         // Process and store the data
         $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
+            'name' => $user_lower,
+            'email' => $email_lower,
             'password' => Hash::make($validatedData['password']),
         ]);
+
 
         // Send confirmation or error messages
         if ($user) {
@@ -106,12 +143,67 @@ class UserController extends Controller
         $posts = DB::table('posts')
             ->join('users', 'posts.author', '=', 'users.email')
             ->where('posts.status', '=', 'Public')
-            ->select('posts.id', 'posts.title', 'posts.body', 'posts.created_at', 'users.name', 'users.picture')
+            ->select('posts.id', 'posts.title', 'posts.body', 'posts.author', 'posts.created_at', 'users.name', 'users.picture', 'users.email')
             ->orderBy('posts.created_at', 'desc')
             ->get();
         $info = ['message' => "Welcome to /Communnity"];
         return view('zencom.community', [
             'posts' => $posts
         ])->with('messagreen', 'Welcome to /Communnity');
+    }
+    public function show_form()
+    {
+        return view('forgot_password');
+    }
+
+
+    public function forgot_password(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email|exists:users',
+        ]);
+        $users = DB::table('users')
+            ->select('*')
+            ->where('email', '=', $request['email'])
+            ->first();
+        if (!$users) {
+            return back()->with('message', 'no email found.');
+        }
+
+        $token = Str::random(64);
+        DB::table('password_resets')->insert([
+            'email' => $request['email'],
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+
+        mail::send(
+            'mails.mailer-forgot',
+            [
+                'users' => $users,
+                'token' => $token
+            ],
+            function ($message) use ($request) {
+                $message->to($request['email']);
+                $message->subject('Forgot Password');
+            }
+
+        );
+
+        return redirect()->to(route("show.forgot"))->with("message", "Check your email for Password reset link");
+    }
+
+    public function password_reset($token)
+    {
+        $users = DB::table('password_resets')
+            ->select('*')
+            ->where('token', '=', $token)
+            ->first();
+        if (!$users) {
+            return back()->with('message', 'Token not found.');
+        } else {
+            return back()->with('message', 'Token found can change pass.');
+        }
     }
 }
